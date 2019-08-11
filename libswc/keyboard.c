ba3f15a9ba5a1b82417b438576fe47427b832345
nia <nia@netbsd.org>
2019-08-11 12:27:57 +0100

    Disable fallocating on NetBSD, it's a TODO item...

--- libswc/keyboard.c
+++ libswc/keyboard.c
@@ -150,10 +150,12 @@ update_keymap(struct xkb *xkb)
 
 	unlink(keymap_path);
 
+#ifndef __NetBSD__
 	if (posix_fallocate(xkb->keymap.fd, 0, xkb->keymap.size) != 0) {
 		WARNING("Could not resize XKB keymap file\n");
 		goto error2;
 	}
+#endif
 
 	xkb->keymap.area = mmap(NULL, xkb->keymap.size, PROT_READ | PROT_WRITE, MAP_SHARED, xkb->keymap.fd, 0);
 
