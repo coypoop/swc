1c80d542b53415c646af91039e37d8b54a3ff475
nia <nia@netbsd.org>
2019-08-12 14:35:57 +0100

    launch: link with -lm

--- launch/local.mk
+++ launch/local.mk
@@ -6,7 +6,7 @@ $(dir)_TARGETS  := $(dir)/swc-launch
 $(dir)_PACKAGES := libdrm
 
 $(dir)/swc-launch: $(dir)/launch.o $(dir)/protocol.o
-	$(link) $(launch_PACKAGE_LIBS)
+	$(link) -lm $(launch_PACKAGE_LIBS)
 
 install-$(dir): $(dir)/swc-launch | $(DESTDIR)$(BINDIR)
 	install -m 4755 launch/swc-launch $(DESTDIR)$(BINDIR)
